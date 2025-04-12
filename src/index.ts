2922a0467592bd8d5c5d6053a18ed37df20ec5ac
mgcodeur <mgcodeur@gmail.com>
2025-04-12 18:01:54 +0300

    feat: get chapters pages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { SCANVF_CONFIG } from './config/scanVf';
-import { ScanVfProvider } from './core/providers/ScanVf';
+import { ScanVfProvider } from '@/core/providers/ScanVf';
 
 async function main(): Promise<void> {
   const pages = await new ScanVfProvider().getManga(`https://www.scan-vf.net/my-hero-academia/`);
